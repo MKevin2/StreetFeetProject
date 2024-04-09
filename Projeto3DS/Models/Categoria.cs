f86dc36952188cd35c0bb9f38f6d58cf12815294
3DS <3ds@bglab.local>
2024-04-09 14:19:08 -0300

    Inserção das classes Tenis e Categoria

---
+++ Projeto3DS/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto3DS.Models
+{
+   public class Categoria
+        {
+        public int categoria_id { get; set; }
+        public string categoria_nome { get; set; }
+        public string desc { get; set; }
+        public List<Tenis> Tenis { get; set; }
+        }
+}
---
+++ Projeto3DS/Models/Tenis.cs
@@ -0,0 +1,18 @@
+﻿namespace Projeto3DS.Models
+{
+    public class Tenis{ 
+        public string desc_curta { get; set; }
+        public string desc_detalhe { get; set; }
+        public bool em_estoque { get; set; }
+        public int tenis_id { get; set; }
+        public string nome { get; set; }
+        public decimal preco { get; set; }
+        public string img_url { get; set; }
+        public string tenis_preferido { get; set; }
+        public string img_miniatura { get; set; }
+        public int categoria_id { get; set; }
+        public virtual Categoria categoria { get; set; }
+    }
+}
+
+
