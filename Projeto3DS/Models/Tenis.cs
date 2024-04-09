840553571143a740436f2c6d1670ee0290d467c7
3DS <3ds@bglab.local>
2024-04-09 14:54:55 -0300

    Inserção da PK

--- Projeto3DS/Models/Tenis.cs
+++ Projeto3DS/Models/Tenis.cs
@@ -1,15 +1,25 @@
-﻿namespace Projeto3DS.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto3DS.Models
 {
+    [Table("Tenis")]
     public class Tenis{ 
         public string desc_curta { get; set; }
         public string desc_detalhe { get; set; }
         public bool em_estoque { get; set; }
+
+        [Key]
         public int tenis_id { get; set; }
         public string nome { get; set; }
         public decimal preco { get; set; }
         public string img_url { get; set; }
         public string tenis_preferido { get; set; }
         public string img_miniatura { get; set; }
+
+        //Verificar se está correto (a Nivia irá verificar)
+        [ForeignKey]
         public int categoria_id { get; set; }
         public virtual Categoria categoria { get; set; }
     }
