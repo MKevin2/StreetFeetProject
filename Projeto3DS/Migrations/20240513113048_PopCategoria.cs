702f61d44b67e9426909c6fa68190d5295b064de
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-05-13 08:40:07 -0300

    Alterações no banco de dados da migration

---
+++ Projeto3DS/Migrations/20240513113048_PopCategoria.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto3DS.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Categoria(categoria_nome, desc) values ('Nike', 'Tênis com as tecnologias Nike')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Categoria");
+        }
+    }
+}
